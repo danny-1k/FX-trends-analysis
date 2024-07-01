7c5de6655f2bb532ec33268ad942c4b18b4b9a6b
Daniel Ik <codingeinstein@gmail.com>
2024-07-01 22:40:46 +0100

    update train_mlp_autoencoder.py

--- src/training/train_mlp_autoencoder.py
+++ src/training/train_mlp_autoencoder.py
@@ -2,10 +2,9 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from models import MLPEncoder, MLPDecoder, Autoencoder
-from data import TrendArrayDataset, TrendImageDataset
-from loss import BCE
-from trainer import Trainer
+from models.models import DenseAutoencoder
+from data .data import TrendImageDataset
+from .trainer import Trainer
 
 
 train = TrendImageDataset(root="../data/processed/images/staggered/train", flatten=True)
@@ -14,16 +13,9 @@ test = TrendImageDataset(root="../data/processed/images/staggered/test", flatten
 train = DataLoader(train, batch_size=128, shuffle=True)
 test = DataLoader(test, batch_size=128, shuffle=True)
 
-input_dim = 128*64
+model = DenseAutoencoder(widths=[64*128, 512, 128])
 
-
-model = Autoencoder(
-    encoder=MLPEncoder(input_dim, [512, 128]),
-    decoder=MLPDecoder(input_dim, widths=[128, 512])
-)
- 
-optimiser = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=.99)
-
+optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 writer = SummaryWriter()
 
@@ -31,15 +23,10 @@ class Loss(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # self.bce = BCE()
-        # self.mse = nn.MSELoss()
-        self.mae = nn.L1Loss()
-
+        self.bce = nn.BCELoss()
     def forward(self, z, p, y):
-        # bce_loss = self.bce(p, y)
-        # mse_loss = self.mse(p, y)
-        mae_loss = self.mae(p, y)
-        return mae_loss
+        bce_loss = self.bce(p, y)
+        return bce_loss
 
 
 lossfn = Loss()
